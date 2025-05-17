3c3b7387753b5e0e9226cf1dcae6b83f6b6f018a
Esethu Mehlo <mehloesethu@gmail.com>
2025-05-17 15:22:03 +0200

    Fourth commit

--- ComputicketCapstone/src/main/java/za/ac/cput/Main.java
+++ ComputicketCapstone/src/main/java/za/ac/cput/Main.java
@@ -4,10 +4,11 @@ import za.ac.cput.domain.Booking;
 import za.ac.cput.factory.BookingFactory;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate date = LocalDate.now();
+        LocalDateTime date = LocalDateTime.now();
 
         Booking b1 = BookingFactory.createBooking("1234", 1234,
                 5667, date," Pending");
